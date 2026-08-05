7bf552a37942fd1844941c61154b08dd7eff3a38
SaksharDaksh <143218367+SaksharDaksh@users.noreply.github.com>
2026-08-05 22:50:28 +0530

    Refactor HeroSection component for improved functionality

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -268,24 +268,6 @@ export default function HeroSection() {
 )}
         </div>
 
-        {/* Scroll Indicator */}
-        <div className="mt-20 animate-bounce">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-        </div>
       </div>
     </section>
   );
